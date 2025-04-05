4b32cd265d22821281aaf874881734cf626d2130
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-04-05 20:29:20 +0900

    split

--- godot_piano/scripts/sound_player.gd
+++ godot_piano/scripts/sound_player.gd
@@ -12,25 +12,36 @@ const BUFFER_LENGTH = 0.5
 # 音の振幅 (0.0 ~ 1.0) - 0.5 は中くらいの音量
 const AMPLITUDE = 0.5
 
+# 再生時間を秒単位で定義
+const DURATION = 2.0
+const FREQUENCY = 440.0
+
+var buffer_size=0
+
 func _ready():
-	pass
+	# バッファサイズを計算
+	buffer_size = int(SAMPLE_RATE * DURATION)
+	
+	# オーディオストリームを作成
+	var audio_stream = AudioStreamGenerator.new()
+	audio_stream.mix_rate = SAMPLE_RATE
+	
+	# AudioStreamPlayer2Dにストリームを設定
+	self.stream = audio_stream
+
 
 func play_c4_sound():
-	# AudioStreamGenerator を作成
-	var audio_stream_generator = AudioStreamGenerator.new()
-	audio_stream_generator.mix_rate = SAMPLE_RATE
-	audio_stream_generator.buffer_length = BUFFER_LENGTH
 
-	# サイン波のデータを生成
-	var buffer_size = int(BUFFER_LENGTH * SAMPLE_RATE)
-	var data = PackedFloat32Array()
+	# 再生
+	self.play()
+	
+	# 1フレーム待機
+	await get_tree().process_frame
+	
+	# オーディオストリーム生成プレイバックを作成
+	var playback = self.get_stream_playback() as AudioStreamGeneratorPlayback
+	
+	# 正弦波データを生成
 	for i in range(buffer_size):
-		var time = float(i) / SAMPLE_RATE
-		var sample = AMPLITUDE * sin(2.0 * PI * C4_FREQUENCY * time)
-		data.push_back(sample)
-
-	# SoundPlayer に生成したストリームを設定
-	stream = audio_stream_generator
-
-	# 音を再生
-	play()
+		var sample_value = sin(2.0 * PI * FREQUENCY * i / SAMPLE_RATE)
+		playback.push_frame(Vector2(sample_value, sample_value))
