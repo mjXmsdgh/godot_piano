0fed7696c1af1ac4340251741e9250a73bedc406
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-04-09 20:26:24 +0900

    update key

--- godot_piano/scripts/key.gd
+++ godot_piano/scripts/key.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
+@onready var sound_player=get_node_or_null("AudioStreamPlayer2D")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if sound_player==null:
+		printerr("AudioStreamPlayer2D node not found!")
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,9 +17,5 @@ func _process(delta: float) -> void:
 func _on_texture_button_button_down() -> void:
 	print("push button")
 	
-	var sound_player=get_node_or_null("AudioStreamPlayer2D")
-	
 	sound_player.set_freq(440)
-	
 	sound_player.play_sound()
-	pass # Replace with function body.
