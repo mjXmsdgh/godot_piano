d9237c3a8da6312cff81c68fd3ed656a129a0c26
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-04-09 20:42:14 +0900

    key a and b

--- godot_piano/scripts/main.gd
+++ godot_piano/scripts/main.gd
@@ -3,7 +3,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Key.set_freq(440)
+	$Key_A.set_freq(440)
+	$Key_B.set_freq(493.88)
 	pass # Replace with function body.
 
 
