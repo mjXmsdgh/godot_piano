1693cb9481ed70587a0e1b3c8ce57e1c12843cbf
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-04-24 20:21:47 +0900

    add option button

---
+++ godot_piano/scripts/chord_option_button.gd
@@ -0,0 +1,18 @@
+extends OptionButton
+
+
+var code_manager:Node=null
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+
+	code_manager=get_node_or_null("../CodeManager")
+
+	add_item("C4")
+	add_item("D4")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
