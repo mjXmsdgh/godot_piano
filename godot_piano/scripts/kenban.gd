178bb0991cfcfa7981eaf55d8fef97e208aeb900
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-05-10 20:07:38 +0900

    add key name

--- godot_piano/scripts/kenban.gd
+++ godot_piano/scripts/kenban.gd
@@ -4,28 +4,54 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Key_A3.set_freq(220.00)   # A3 (ラ) - A4の1オクターブ下
+	$Key_A3.set_keyname("A3")
+
 	$Key_As3.set_freq(233.08)  # A#3 / Bb3 (ラ# / シ♭)
+	$Key_As3.set_keyname("As3")
 	$Key_B3.set_freq(246.94)   # B3 (シ)
+	$Key_B3.set_keyname("B3")
 	$Key_C4.set_freq(261.63)   # C4 (ド)
+	$Key_C4.set_keyname("C4")
 	$Key_Cs4.set_freq(277.18)  # C#4 / Db4 (ド# / レ♭)
+	$Key_Cs4.set_keyname("Cs4")
 	$Key_D4.set_freq(293.66)   # D4 (レ)
+	$Key_D4.set_keyname("D4")
 	$Key_Ds4.set_freq(311.13)  # D#4 / Eb4 (レ# / ミ♭)
+	$Key_Ds4.set_keyname("Ds4")
 	$Key_E4.set_freq(329.63)   # E4 (ミ)
+	$Key_E4.set_keyname("E4")
 	$Key_F4.set_freq(349.23)   # F4 (ファ)
+	$Key_F4.set_keyname("F4")
 	$Key_Fs4.set_freq(369.99)  # F#4 / Gb4 (ファ# / ソ♭)
+	$Key_Fs4.set_keyname("Fs4")
 	$Key_G4.set_freq(392.00)   # G4 (ソ)
+	$Key_G4.set_keyname("G4")
 	$Key_Gs4.set_freq(415.30)  # G#4 / Ab4 (ソ# / ラ♭)
+	$Key_Gs4.set_keyname("Gs4")
 	$Key_A4.set_freq(440.00)   # A4 (ラ) - 基準周波数
+	$Key_A4.set_keyname("A4")
 	$Key_As4.set_freq(466.16)  # A#4 / Bb4 (ラ# / シ♭)
+	$Key_As4.set_keyname("As4")
 	$Key_B4.set_freq(493.88)   # B4 (シ)
+	$Key_B4.set_keyname("B4")
 	$Key_C5.set_freq(523.25)   # C5 (ド) - 中央ド
+	$Key_C5.set_keyname("C5")
 	$Key_Cs5.set_freq(554.37)  # C#5 / Db5 (ド# / レ♭)
+	$Key_Cs5.set_keyname("Cs5")
 	$Key_D5.set_freq(587.33)   # D5 (レ)
+	$Key_D5.set_keyname("D5")
 	$Key_Ds5.set_freq(622.25)  # D#5 / Eb5 (レ# / ミ♭)
+	$Key_Ds5.set_keyname("Ds5")
 	$Key_E5.set_freq(659.26)   # E5 (ミ)
+	$Key_E5.set_keyname("E5")
 	$Key_F5.set_freq(698.46)   # F5 (ファ)
+	$Key_F5.set_keyname("F5")
 	$Key_Fs5.set_freq(739.9)
+	$Key_Fs5.set_keyname("Fs5")
 	$Key_G5.set_freq(783.99)   # G5 (ソ) - 中央ソ
+	$Key_G5.set_keyname("G5")
+
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- godot_piano/scripts/key.gd
+++ godot_piano/scripts/key.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var sound_player=get_node_or_null("AudioStreamPlayer2D")
 @onready var reset_timer=$Timer
 var original_texture_normal: Texture2D
+var key_name="none"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,16 +21,18 @@ func _process(delta: float) -> void:
 func set_freq(input_freq) -> void:
 	sound_player.set_freq(input_freq)
 
+func set_keyname(input_key_name) -> void :
+	key_name=input_key_name
 
 func _on_texture_button_button_down() -> void:
 	#sound_player.play_sound()
-
 	play_sound()
 
 
 func play_sound() -> void:
 	if sound_player:
 		sound_player.play_sound()
+		print(key_name)
 
 		$TextureButton.texture_normal=$TextureButton.texture_pressed
 		reset_timer.start()
@@ -40,6 +43,5 @@ func play_sound() -> void:
 func _on_timer_timeout() -> void:
 
 	$TextureButton.texture_normal=original_texture_normal
-
 
 	pass # Replace with function body.
