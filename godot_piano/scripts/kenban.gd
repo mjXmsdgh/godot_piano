314bc8ed8fac3ee2c0d69526b597fa7e62e726de
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-05-22 20:32:57 +0900

    change kenban.gd by gemini

--- godot_piano/scripts/kenban.gd
+++ godot_piano/scripts/kenban.gd
@@ -3,103 +3,55 @@ extends Node2D
 # 各鍵盤ノードを音名と関連付けて格納するための辞書
 var notes: Dictionary = {}
 
+	
+# 各鍵盤の情報をまとめた配列を定義
+var key_data = [
+	{"note_name": "A3", "frequency": 220.00, "node_path": "Key_A3"},
+	{"note_name": "A#3", "frequency": 233.08, "node_path": "Key_As3"},
+	{"note_name": "B3", "frequency": 246.94, "node_path": "Key_B3"},
+	{"note_name": "C4", "frequency": 261.63, "node_path": "Key_C4"},
+	{"note_name": "C#4", "frequency": 277.18, "node_path": "Key_Cs4"},
+	{"note_name": "D4", "frequency": 293.66, "node_path": "Key_D4"},
+	{"note_name": "D#4", "frequency": 311.13, "node_path": "Key_Ds4"},
+	{"note_name": "E4", "frequency": 329.63, "node_path": "Key_E4"},
+	{"note_name": "F4", "frequency": 349.23, "node_path": "Key_F4"},
+	{"note_name": "F#4", "frequency": 369.99, "node_path": "Key_Fs4"},
+	{"note_name": "G4", "frequency": 392.00, "node_path": "Key_G4"},
+	{"note_name": "G#4", "frequency": 415.30, "node_path": "Key_Gs4"},
+	{"note_name": "A4", "frequency": 440.00, "node_path": "Key_A4"},
+	{"note_name": "A#4", "frequency": 466.16, "node_path": "Key_As4"},
+	{"note_name": "B4", "frequency": 493.88, "node_path": "Key_B4"},
+	{"note_name": "C5", "frequency": 523.25, "node_path": "Key_C5"},
+	{"note_name": "C#5", "frequency": 554.37, "node_path": "Key_Cs5"},
+	{"note_name": "D5", "frequency": 587.33, "node_path": "Key_D5"},
+	{"note_name": "D#5", "frequency": 622.25, "node_path": "Key_Ds5"},
+	{"note_name": "E5", "frequency": 659.26, "node_path": "Key_E5"},
+	{"note_name": "F5", "frequency": 698.46, "node_path": "Key_F5"},
+	{"note_name": "F#5", "frequency": 739.90, "node_path": "Key_Fs5"}, # 周波数の . を追加
+	{"note_name": "G5", "frequency": 783.99, "node_path": "Key_G5"}
+]
+
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Key_A3.set_freq(220.00)   # A3 (ラ) - A4の1オクターブ下
-	$Key_A3.set_keyname("A3")
-	notes["A3"] = $Key_A3
 
-	$Key_As3.set_freq(233.08)  # A#3 / Bb3 (ラ# / シ♭)
-	$Key_As3.set_keyname("A#3")
-	notes["A#3"] = $Key_As3
+	for data in key_data:
+		var note_name: String = data.note_name
+		var frequency: float = data.frequency
+		var node_path: String = data.node_path
+		
+		var key_node = get_node(node_path)
 
-	$Key_B3.set_freq(246.94)   # B3 (シ)
-	$Key_B3.set_keyname("B3")
-	notes["B3"] = $Key_B3
-	
-	$Key_C4.set_freq(261.63)   # C4 (ド)
-	$Key_C4.set_keyname("C4")
-	notes["C4"] = $Key_C4
+		if !key_node:
+			printerr("Error: Node not found at path: %s for note %s" % [node_path, note_name])
 
-	$Key_Cs4.set_freq(277.18)  # C#4 / Db4 (ド# / レ♭)
-	$Key_Cs4.set_keyname("C#4")
-	notes["C#4"] = $Key_Cs4
+		key_node.set_freq(frequency)
+		key_node.set_keyname(note_name)
+		notes[note_name] = key_node
+			
 
-	$Key_D4.set_freq(293.66)   # D4 (レ)
-	$Key_D4.set_keyname("D4")
-	notes["D4"] = $Key_D4
-
-	$Key_Ds4.set_freq(311.13)  # D#4 / Eb4 (レ# / ミ♭)
-	$Key_Ds4.set_keyname("D#4")
-	notes["D#4"] = $Key_Ds4
-
-	$Key_E4.set_freq(329.63)   # E4 (ミ)
-	$Key_E4.set_keyname("E4")
-	notes["E4"] = $Key_E4
-
-	$Key_F4.set_freq(349.23)   # F4 (ファ)
-	$Key_F4.set_keyname("F4")
-	notes["F4"] = $Key_F4
-
-	$Key_Fs4.set_freq(369.99)  # F#4 / Gb4 (ファ# / ソ♭)
-	$Key_Fs4.set_keyname("F#4")
-	notes["F#4"] = $Key_Fs4
-
-	$Key_G4.set_freq(392.00)   # G4 (ソ)
-	$Key_G4.set_keyname("G4")
-	notes["G4"] = $Key_G4
-
-	$Key_Gs4.set_freq(415.30)  # G#4 / Ab4 (ソ# / ラ♭)
-	$Key_Gs4.set_keyname("G#4")
-	notes["G#4"] = $Key_Gs4
-
-	$Key_A4.set_freq(440.00)   # A4 (ラ) - 基準周波数
-	$Key_A4.set_keyname("A4")
-	notes["A4"] = $Key_A4
-
-
-	$Key_As4.set_freq(466.16)  # A#4 / Bb4 (ラ# / シ♭)
-	$Key_As4.set_keyname("A#4")
-	notes["A#4"] = $Key_As4
-
-	$Key_B4.set_freq(493.88)   # B4 (シ)
-	$Key_B4.set_keyname("B4")
-	notes["B4"] = $Key_B4
-
-	$Key_C5.set_freq(523.25)   # C5 (ド) - 中央ド
-	$Key_C5.set_keyname("C5")
-	notes["C5"] = $Key_C5
-
-	$Key_Cs5.set_freq(554.37)  # C#5 / Db5 (ド# / レ♭)
-	$Key_Cs5.set_keyname("C#5")
-	notes["C#5"] = $Key_Cs5
-
-	$Key_D5.set_freq(587.33)   # D5 (レ)
-	$Key_D5.set_keyname("D5")
-	notes["D5"] = $Key_D5
-
-
-	$Key_Ds5.set_freq(622.25)  # D#5 / Eb5 (レ# / ミ♭)
-	$Key_Ds5.set_keyname("D#5")
-	notes["D#5"] = $Key_Ds5
-
-	$Key_E5.set_freq(659.26)   # E5 (ミ)
-	$Key_E5.set_keyname("E5")
-	notes["E5"] = $Key_E5
-
-	$Key_F5.set_freq(698.46)   # F5 (ファ)
-	$Key_F5.set_keyname("F5")
-	notes["F5"] = $Key_F5
-
-	$Key_Fs5.set_freq(739.9)
-	$Key_Fs5.set_keyname("F#5")
-	notes["F#5"] = $Key_Fs5
-
-	$Key_G5.set_freq(783.99)   # G5 (ソ) - 中央ソ
-	$Key_G5.set_keyname("G5")
-	notes["G5"] = $Key_G5
-
-	
 
 func play_note(note_name: String) -> void:
 	var key_nodes=notes[note_name]
