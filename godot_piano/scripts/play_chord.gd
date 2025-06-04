c8a91e40f43c735addca2f0b54eef2e84028cad8
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-06-04 20:34:26 +0900

    add type hint and comment

--- godot_piano/scripts/play_chord.gd
+++ godot_piano/scripts/play_chord.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
+# コードデータを管理するノード
 @onready var code_manager: Node = get_node_or_null("../CodeManager")
+# ピアノの音を再生するノード (ルートノードを想定)
 @onready var piano_player: Node = get_node_or_null("../")
+# コードを選択するOptionButtonノード
 @onready var chord_option_button = get_node_or_null("../chord_OptionButton")
 
 # _ready() はノード取得以外の初期化処理がなければ削除できます
@@ -11,14 +14,18 @@ func _ready() -> void:
 
 func _on_play_chord_pressed() -> void:
 
-	var chord_to_play=chord_option_button.get_chord()
+	# OptionButtonから選択されたコード名を取得
+	var chord_to_play: String = chord_option_button.get_chord()
 
-	#var chord_to_play:String="C"
-	var chord_data=code_manager.chord_data
-	var chord_info=chord_data[chord_to_play]
+	# CodeManagerから全体のコードデータを取得
+	var chord_data: Dictionary = code_manager.chord_data
 
-	var notes_array=chord_info["notes"]
+	# 選択されたコード名に対応する情報を取得
+	var chord_info: Dictionary = chord_data[chord_to_play]
 
+	# コード情報から音符の配列を取得
+	var notes_array: Array = chord_info["notes"]
+
+	# 配列内の各音符を再生
 	for note in notes_array:
-
 		piano_player.play_note(note)
